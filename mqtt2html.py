02eb0818bd033fd11bf8ca67024c145c6e5ac890
Marco Bakera <marco@bakera.de>
2025-02-16 17:55:31 +0100

    Refactor mqtt2html.py to improve MQTT connection handling and topic subscription from configuration

--- mqtt2html.py
+++ mqtt2html.py
@@ -1,4 +1,4 @@
-from paho.mqtt.client import Client
+import paho.mqtt.client as mqtt
 import sys
 import configparser
 import time
@@ -7,9 +7,16 @@ from threading import Thread
 CONFIG_FILE = "config.ini"
 messages = {}
 
-def on_connect(client, userdata, flags, rc):
-    print("Verbunden mit Status", rc)
-    # client.subscribe("#")  # Auskommentiert, da jetzt Konfiguration genutzt wird
+config = configparser.ConfigParser()
+print("Lese Konfiguration aus", CONFIG_FILE)
+config.read(CONFIG_FILE)
+
+def on_connect(client, userdata, flags, reason_code, properties):
+    print("Verbindung hergestellt")
+    topics = [t.strip() for t in config["mqtt"]["topics"].split(",")]
+    print("Abonniere folgende Topics:", topics)
+    for t in topics:
+        client.subscribe(t)
 
 def on_message(client, userdata, msg):
     messages[msg.topic] = msg.payload.decode("utf-8")
@@ -24,22 +31,16 @@ def export_to_html():
             f.write("</table></body></html>")
 
 def main():
-    config = configparser.ConfigParser()
-    print("Lese Konfiguration aus", CONFIG_FILE)
-    config.read(CONFIG_FILE)
-
     host = config["mqtt"]["host"]
     port = int(config["mqtt"]["port"])
-    topics = [t.strip() for t in config["mqtt"]["topics"].split(",")]
 
-    client = Client()
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_connect = on_connect
     client.on_message = on_message
 
+    print("Verbinde zu", host, "auf Port", port)
     client.connect(host, port, 60)
 
-    for t in topics:
-        client.subscribe(t)
 
     Thread(target=export_to_html, daemon=True).start()
     client.loop_forever()
